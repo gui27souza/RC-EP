9ae8c07642b8c1049927a6acb11b8b117ca00caf
Guilherme27Souza <guilhermeoliveiradesouza@gmail.com>
2025-10-13 11:37:16 -0300

    funcao do server para iniciar jogador mestre e receber palavra

---
+++ server_funcs/master_setup.py
@@ -0,0 +1,46 @@
+import random, socket
+
+from typing import List, Tuple, Dict
+
+from ..funcs.receive_message import receive_message
+from .send_message_to_all import send_message_to_all
+
+def master_setup(connected_players: List[Dict]) -> Tuple[Dict, str]:
+    
+    master_player = random.choice(connected_players)
+    master_socket = master_player['socket']
+
+    print(f"Jogador mestre: {master_player['name']}")
+
+    try:
+        master_socket.sendall("MASTER\r\n".encode('ascii'))
+        response = receive_message(master_socket)
+
+        if response and response.startswith("WORD"):
+            try:
+                chosen_word = response.split(' ', 1)[1].strip()
+                if _is_word_valid(chosen_word):
+                    master_socket.sendall("OK\r\n".encode('ascii'))
+                    print(f"Jogador mestre forneceu a palavra: {chosen_word}")
+                    return master_player, chosen_word.upper()
+                else: raise ValueError("Palavra inválida")
+            except (IndexError, ValueError):
+                raise ValueError("Mensagem WORD mal-formatada ou palavra inválida")
+
+    except Exception as e:
+        
+        print(f"Erro CRÍTICO no setup do mestre ({master_player['name']}): {e}")
+        send_message_to_all(connected_players, "ERROR INVALID_MASTER_MESSAGE")
+
+        for p in connected_players: 
+            try: p['socket'].close()
+            except: pass
+        
+        return None, None
+
+def _is_word_valid(word: str) -> bool:
+    return (
+        not '-' in word   and\
+        len(word) > 0     and\
+        word.isalpha()
+    )
