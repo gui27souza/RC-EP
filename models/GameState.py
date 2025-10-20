f66e8d8bf0b5d38b7e0efda2ca952dfb598eb34f
Guilherme27Souza <137806447+gui27souza@users.noreply.github.com>
2025-10-20 10:55:35 -0300

    reformula classe GameState

--- models/GameState.py
+++ models/GameState.py
@@ -1,12 +1,32 @@
 from dataclasses import dataclass, field
 
 from typing import List
+from .Player import Player
 
 @dataclass
 class GameState:
 
     word: str
-    word_array: List[str]
-    word_progress: List[str]
-    lives: 7
-    guesses: List[str]
+    all_players: List[Player]
+    master_player: Player
+
+    lives: int = 7
+    guesses: List[str] = []
+
+    word_array: List[str] = field(init=False)
+    word_progress: List[str] = field(init=False)
+    common_players: List[Player] = field(init=False)
+
+    def __post_init__(self):
+        
+        word_array = []
+        empty_word_array = []
+
+        for letter in self.word:
+            word_array.append(letter.upper())
+            empty_word_array.append('_')
+        common_players = [player for player in self.all_players if player != self.master_player]
+
+        self.word_array = word_array
+        self.word_progress = empty_word_array
+        self.common_players = common_players
