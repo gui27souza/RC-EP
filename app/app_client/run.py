9f182f699ebeb912793259b7d11dee4edf3c70b1
Guilherme27Souza <137806447+gui27souza@users.noreply.github.com>
2025-10-26 20:28:09 -0300

    rascunho do cliente

--- app/app_client/run.py
+++ app/app_client/run.py
@@ -1,6 +1,7 @@
 import socket
 
 from . import client
+from app.models import ClientMessage, GameState
 
 def run_game():
 
@@ -12,5 +13,52 @@ def run_game():
         socket.SOCK_STREAM  # especifica que o transporte será TCP
     )
 
+    # Conecta com o servidor pelo socket
     client.server_conn.connect_to_server(client_socket, ip, porta)
 
+    # Envia mensagem de anuncio de novo player, com o nome do jogador
+    ClientMessage.send_message_to_server(client_socket, ClientMessage.NEWPLAYER(nome_jogador))
+
+    while True:
+        
+        response = ClientMessage.receive_message_from_server(client_socket)
+
+        ### ERROR
+        if response.startswith("ERROR"):
+
+            pass
+
+        ### STANDBY
+        elif response.startswith("STANDBY"):
+            
+            pass
+
+        ### MASTER ###
+        elif response.startswith("MASTER"):
+            
+            pass
+
+        ### NEWGAME
+        elif response.startswith("NEWGAME"):
+            
+            pass
+
+        ### YOURTURN
+        elif response.startswith("YOURTURN"):
+
+            pass
+
+        ### STATUS
+        elif response.startswith("STATUS"):
+
+            pass
+
+        ### GAMEOVER
+        elif response.startswith("GAMEOVER"):
+
+            pass
+
+        ### MENSAGEM INESPERADA
+        else:
+
+            pass
