f94e603f0ac14473aed01c35e922f27950076f7d
Guilherme27Souza <guilhermeoliveiradesouza@gmail.com>
2025-10-30 08:51:36 -0300

    dev: script para teste

---
+++ test_game.zsh
@@ -0,0 +1,25 @@
+#!/bin/zsh
+
+# --- CONFIGURAÇÕES ---
+PYTHON_CMD="python3"
+SERVER_APP="app.app_server.main" # Assumindo que seu main.py está em app_server
+CLIENT_APP="app.app_client.main"
+NUM_JOGADORES=3
+PORTA=6891
+IP="127.0.0.1"
+TERMINAL="gnome-terminal"
+
+echo "Iniciando Servidor e 3 Clientes..."
+
+# 1. INICIAR O SERVIDOR
+# Note o uso de aspas simples para encapsular o comando completo:
+$TERMINAL --title="Servidor Forca" --command="$PYTHON_CMD -m $SERVER_APP $NUM_JOGADORES $PORTA" &
+
+sleep 2
+
+# 2. INICIAR OS CLIENTES
+$TERMINAL --title="Cliente Alice" --command="$PYTHON_CMD -m $CLIENT_APP Alice $IP:$PORTA" &
+$TERMINAL --title="Cliente Bob" --command="$PYTHON_CMD -m $CLIENT_APP Bob $IP:$PORTA" &
+$TERMINAL --title="Cliente Carla" --command="$PYTHON_CMD -m $CLIENT_APP Carla $IP:$PORTA" &
+
+echo "Prontos! Verifique as novas janelas do terminal."
