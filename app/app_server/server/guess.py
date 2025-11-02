dc7567864c58f7d324c9eb580796c387e47f8be0
Guilherme27Souza <guilhermeoliveiradesouza@gmail.com>
2025-11-02 13:56:26 -0300

    feat: valida e trata erros de palpite do jogador do lado do servidor

--- app/app_server/server/guess.py
+++ app/app_server/server/guess.py
@@ -4,26 +4,42 @@ from app.models import Player, ServerGameState, ServerMessage, Error
 from app.debug import print_debug
 
 def deal_guess(turn_player: Player, game_state: ServerGameState, guess_response: str) -> str:
+    """
+    Lida com palpite do jogador. Também lida com erros e informa ao cliente.
+    """
 
+    # Valida o palpite, retornando o tipo, o palpite validado e, caso ocorra, o tipo do erro
     guess_type, guess_str, guess_error = _validate_guess(guess_response, game_state)
-    
-    if guess_error:
-        print_debug(f"Palpite inválido de {turn_player.name}: {guess_type} {guess_str}\n{guess_error}")
-        ServerMessage.send_message_to_player(turn_player, guess_error)
-        return None
-    print_debug(f"Palpite válido de {turn_player.name}: {guess_type} {guess_str}")
 
+    # Informa ao jogador caso algum erro ocorra
+    if guess_error:
+
+        print_debug(f"Palpite inválido de {turn_player.name}: {guess_type} {guess_str}\n{guess_error}")
+
+        ServerMessage.send_message_to_player(turn_player, guess_error)
+
+        if guess_error == Error.ALREADY_GUESSED: 
+            return guess_str
+        else:
+            return None
+
+
+    # Logs informativos
     if guess_type == "WORD": guess_type_msg = "palavra"
     if guess_type == "LETTER": guess_type_msg = "letra"
-
     print(f"Processando palpite de {guess_type_msg}: '{guess_str}'")
+
+    # Processa o palpite
     _process_guess(guess_type, guess_str, game_state)
+
+    # Avisa o jogador de que tudo ocorreu como esperado
     ServerMessage.send_message_to_player(turn_player, "OK")
+
     return guess_str
 
 
-# --------------- Funções Auxiliares ---------------
 
+# =============== Funções Auxiliares ===============
 
 def _validate_guess(guess: str, game_state: ServerGameState) -> Tuple[str, str, str|None]:
     """
@@ -38,7 +54,7 @@ def _validate_guess(guess: str, game_state: ServerGameState) -> Tuple[str, str, 
             guess_type = "LETTER"
 
             validated_guess, guess_error = _validate_guess_letter(guess, game_state)
-        
+
         elif guess.startswith("WORD "):
             guess = guess[5:].strip().upper()
             guess_type = "WORD"
@@ -48,7 +64,7 @@ def _validate_guess(guess: str, game_state: ServerGameState) -> Tuple[str, str, 
         else: return None, None, Error.INVALID_FORMAT
 
         return guess_type, validated_guess, guess_error 
-    
+
     return None, None, Error.UNEXPECTED_MESSAGE
 
 def _validate_guess_letter(guess: str, game_state: ServerGameState) -> Tuple[str, str|None]:
@@ -57,11 +73,18 @@ def _validate_guess_letter(guess: str, game_state: ServerGameState) -> Tuple[str
     Retorna o palpite e o erro caso não seja válido
     """
 
-    if len(guess) != 1 or not guess.isalpha():
-        return guess, Error.INVALID_LETTER
+    if len(guess) != 1:
+        return guess, Error.INVALID_FORMAT
     
-    if guess in game_state.guesses: return guess, Error.ALREADY_GUESSED
-        
+    if not guess.isalpha():
+        return guess, Error.INVALID_LETTER
+
+    if (
+        guess in game_state.guesses or
+        guess in game_state.word_progress
+    ):
+        return guess, Error.ALREADY_GUESSED
+
     return guess, None
 
 def _validate_guess_word(guess: str, game_state: ServerGameState) -> Tuple[str, str|None]:
@@ -69,7 +92,7 @@ def _validate_guess_word(guess: str, game_state: ServerGameState) -> Tuple[str, 
     Valida palpilte de palavra. 
     Retorna o palpite e o erro caso não seja válido
     """
-    
+
     if not guess.isalpha() or len(guess.split(' ')) != 1: 
         return guess, Error.INVALID_FORMAT
 
@@ -84,23 +107,36 @@ def _validate_guess_word(guess: str, game_state: ServerGameState) -> Tuple[str, 
 
 
 def _process_guess(guess_type: str, guess: str, game_state: ServerGameState):
-    
+    """
+    Processa o palpite, verificando se acertou ou errou, fazendo as devidas atualizações
+    """
+
     if guess_type == "LETTER":
+
+        # Palpite correto
         if guess in game_state.word_array:
             for i in range(0, len(game_state.word_array)):
                 if guess == game_state.word_array[i]:
                     game_state.word_progress[i] = guess
             print_debug("Letra estava na palavra!")
+
+        # Palpite incorreto
         else:
             game_state.lives -= 1
+            game_state.guesses.append(guess)
             print_debug("Letra não estava na palavra!")
-        
+
 
     if guess_type == "WORD":
+
+        # Palpite correto
         if guess == game_state.word:
             for i in range(0, len(game_state.word_array)):
                 game_state.word_progress[i] = guess[i]
             print_debug("Palavra acertada!")
+
+        # Palpite incorreto
         else:
             game_state.lives -= 1
+            game_state.guesses.append(guess)
             print_debug("Palavra não acertada!")
